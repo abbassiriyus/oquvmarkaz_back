879e32f9f7254e73021cfe033f6c0273bfd7bb39
Akbar-07 <86982819+abbassiriyus@users.noreply.github.com>
2023-08-14 12:16:04 +0300

    zxc
    czx

--- lesson/attendance_test.js
+++ lesson/attendance_test.js
@@ -31,8 +31,8 @@ router.get('/attendance_test/:id', (req, res) => {
 
 router.post("/attendance_test",ensureToken, (req, res) => {
     const body = req.body;
-        pool.query('INSERT INTO attendance_test (lesson_id,group_id,mark,came) VALUES ($1,$2,$3,$4) RETURNING *',
-        [body.lesson_id,body.group_id,body.mark,body.came],
+        pool.query('INSERT INTO attendance_test (test_id,group_id,mark,came) VALUES ($1,$2,$3,$4) RETURNING *',
+        [body.test_id,body.group_id,body.mark,body.came],
          (err, result) => {
             if (err) {
                 res.status(400).send(err);
