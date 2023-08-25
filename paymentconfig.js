6ea6d3f9dc36700cc1e47c91132a28c4af4b8c7c
Akbar-07 <86982819+abbassiriyus@users.noreply.github.com>
2023-08-25 08:14:12 +0300

    qwe
    ewq

--- paymentconfig.js
+++ paymentconfig.js
@@ -8,7 +8,7 @@ router.get('/pay',(req,res)=>{
 
     fs.readFile('./configpayment.txt',{encoding: 'utf-8'},(err,data)=>{
    if(!err){
-res.status(200).send(data)
+res.status(200).send(JSON.parse(data))
    }else{
     res.status(400).send(err)
    }
@@ -17,7 +17,25 @@ res.status(200).send(data)
 })
 
 
+router.put('/pay',(req,res)=>{
+var body=req.body
+    fs.readFile('./configpayment.txt',{encoding: 'utf-8'},(err,data)=>{
+        if(!err){
+            var data2=JSON.parse(data)
+var put={
+    "public_key":body.public_key?body.public_key:data2.public_key,
+    "strip_key":body.strip_key?body.strip_key:data2.strip_key,
+    "paypal_kluch":body.paypal_kluch?body.paypal_kluch:data2.paypal_kluch
+}
+fs.writeFileSync('./configpayment.txt',JSON.stringify(put),{encoding: 'utf-8'})
+res.status(200).send("update")
 
+        }else{
+         res.status(400).send(err)
+        }
+             
+         })
+})
 
 
 module.exports = router;
