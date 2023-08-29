0b3bae4b6d82767fc20a9b9f344748030c8ecd05
Akbar-07 <86982819+abbassiriyus@users.noreply.github.com>
2023-08-29 16:53:37 +0300

    qwer
    weqr

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 require("dotenv").config()
 var STRIPE_SECRET_TEST=JSON.parse(fs.readFileSync("./configpayment.txt",{encoding: 'utf-8'})).strip_key
+var API_KEY=JSON.parse(fs.readFileSync("./configpayment.txt",{encoding: 'utf-8'})).daily_API_KEY
 const stripe = require("stripe")(STRIPE_SECRET_TEST)
 const pool = require("./db")
 const user=require('./routes/user.js')
@@ -56,6 +57,58 @@ app.use(express.static('./routes/Images'))
 app.use(bodyParser.json());
 
 
+
+const headers = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+  Authorization: "Bearer " + API_KEY,
+};
+
+const getRoom = (room) => {
+  return fetch(`https://api.daily.co/v1/rooms/${room}`, {
+    method: "GET",
+    headers,
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      return json;
+    })
+    .catch((err) => console.error("error:" + err));
+};
+
+const createRoom = (room) => {
+  return fetch("https://api.daily.co/v1/rooms", {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      name: room,
+      properties: {
+        enable_screenshare: true,
+        enable_chat: true,
+        start_video_off: true,
+        start_audio_off: false,
+        lang: "en",
+      },
+    }),
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      return json;
+    })
+    .catch((err) => console.log("error:" + err));
+};
+
+app.get("/video-call/:id", async function (req, res) {
+  const roomId = req.params.id;
+
+  const room = await getRoom(roomId);
+  if (room.error) {
+    const newRoom = await createRoom(roomId);
+    res.status(200).send(newRoom);
+  } else {
+    res.status(200).send(room);
+  }
+});
 
 app.post("/payment", cors(), async (req, res) => {
   let { amount, id } = req.body
