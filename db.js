21895abb05afcbd2fbe8886092dd2f8c281f9bad
Akbar-07 <86982819+abbassiriyus@users.noreply.github.com>
2023-09-07 16:34:38 +0300

    asd
    asd

--- db.js
+++ db.js
@@ -1,5 +1,12 @@
 const { Client } = require("pg")
-
+// const pool = new Client({
+//     user: "abbasuz1_admin",
+//     host: "localhost",
+//     database: "	abbasuz1_dastafka",
+//     // password: "ix93ZOu,ZBz=",
+//     password: "_v8bDMy,,LKo",
+//     port: 5432
+// })
 const pool = new Client({
     user: "postgres",
     host: "containers-us-west-76.railway.app",
