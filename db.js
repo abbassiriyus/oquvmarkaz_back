25d6c2c5f66d1559d251a6f905be82dc9b8613fd
Flander404 <130212163+ARTXxXotw@users.noreply.github.com>
2023-09-09 18:21:58 +0500

    asd
    asd

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 const { Client } = require("pg")
-const pool = new Client({
-    user: "abbasuz1_admin",
-    host: "localhost",
-    database: "	abbasuz1_dastafka",
-    // password: "ix93ZOu,ZBz=",
-    password: "_v8bDMy,,LKo",
-    port: 5432
-})
 // const pool = new Client({
-//     user: "postgres",
-//     host: "containers-us-west-76.railway.app",
-//     database: "railway",
-//     password: "jkqTVujoML9q8kAOgpDh",
-//     port: 7550
+//     user: "abbasuz1_admin",
+//     host: "localhost",
+//     database: "	abbasuz1_dastafka",
+//     // password: "ix93ZOu,ZBz=",
+//     password: "_v8bDMy,,LKo",
+//     port: 5432
 // })
+const pool = new Client({
+    user: "postgres",
+    host: "containers-us-west-76.railway.app",
+    database: "railway",
+    password: "jkqTVujoML9q8kAOgpDh",
+    port: 7550
+})
 pool.connect(err => {
     if(err) {
         console.log("Connect Error");
